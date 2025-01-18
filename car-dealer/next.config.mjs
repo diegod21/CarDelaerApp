45c7ed5e0ea33fda7531b81161bfacb24ac2e3c5
di <diego@apicesistemas.com.br>
2025-01-18 11:33:35 -0300

    upping

--- car-dealer/next.config.mjs
+++ car-dealer/next.config.mjs
@@ -1,5 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    assetPrefix: '/',
+    basePath: '/CarDealerApp',
+    trailingSlash: true,
+    output: "export",  // <=== habilita o export estático
+    reactStrictMode: true,
 };
+
 
 export default nextConfig;
